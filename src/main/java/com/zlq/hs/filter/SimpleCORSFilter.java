113533b9a070b31a25d5a3f7f5148301f362ea76
18932540203 <Lin630790liN>
2019-07-08 17:34:55 +0800

    2019年7月8日第二次提交

--- src/main/java/com/zlq/hs/filter/SimpleCORSFilter.java
+++ src/main/java/com/zlq/hs/filter/SimpleCORSFilter.java
@@ -1,10 +1,44 @@
 package com.zlq.hs.filter;
 
-import java.util.logging.Filter;
-import java.util.logging.LogRecord;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class SimpleCORSFilter implements Filter {
-    public boolean isLoggable(LogRecord record) {
-        return false;
+    private boolean isCross = false;
+
+    public void destroy() {
+        isCross = false;
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+        if (isCross) {
+            res.setHeader("Access-Control-Allow-Origin",req.getHeader("Origin"));
+            if(req.getHeader("Origin")!=null&&!"".equals(req.getHeader("Origin"))){
+                res.setHeader("Access-Control-Allow-Origin",req.getHeader("Origin"));
+            }else{
+                res.setHeader("Access-Control-Allow-Origin","*");
+            }
+            res.setHeader("Access-Control-Allow-Credentials", "true");
+            res.setHeader("Access-Control-Allow-Headers",
+                    "Accept,Accept-Encoding,Accept-Language,Connection,Content-Length,Content-Type, Cookie,Host,Origin,Referer,User-Agent, Authorization,X-Requested-With,OsType");
+            res.setHeader("Access-Control-Allow-Methods", "POST,GET, HEAD,PUT, DELETE, TRACE, OPTIONS");
+        }
+        if("OPTIONS".equalsIgnoreCase(req.getMethod())){
+            response.getOutputStream().close();
+        }else{
+
+            chain.doFilter(req, res);
+        }
+    }
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        String isCrossStr = filterConfig.getInitParameter("IsCross");
+        isCross = isCrossStr.equals("true") ? true : false;
     }
 }
